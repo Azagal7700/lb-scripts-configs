7db443c65350167789154283b8f7f107e9e156e6
Kadir <azagal7700@protonmail.com>
2025-01-26 22:08:48 +0100

    up phone

--- lb-phone/client/custom/functions/functions.lua
+++ lb-phone/client/custom/functions/functions.lua
@@ -137,3 +137,67 @@ exports("SetServiceBars", function(bars)
 
     staticService = bars
 end)
+
+function ReloadPhone()
+    local wasOpen = phoneOpen
+
+    debugprint("ReloadPhone triggered")
+
+    LogOut()
+    Wait(1000)
+    FetchPhone()
+
+    if wasOpen then
+        ToggleOpen(true)
+    end
+end
+
+exports("ReloadPhone", ReloadPhone)
+
+local phoneVariation
+
+---Check if the player has a phone
+---@return boolean
+function HasPhoneItem(number)
+    if not Config.Item.Require then
+        return true
+    end
+
+    if Config.Item.Unique then
+        return HasPhoneNumber(number)
+    end
+
+    if Config.Item.Name then
+        return HasItem(Config.Item.Name)
+    end
+
+    if phoneVariation and HasItem(Config.Item.Names[phoneVariation].name) then
+        return true
+    end
+
+    if not phoneVariation then
+        local storedVariation = GetResourceKvpInt("phone_variation")
+
+        if storedVariation and Config.Item.Names[storedVariation] and HasItem(Config.Item.Names[storedVariation].name) then
+            phoneVariation = storedVariation
+
+            SetPhoneVariation(storedVariation)
+
+            return true
+        end
+    end
+
+    for i = 1, #Config.Item.Names do
+        local item = Config.Item.Names[i]
+
+        if HasItem(item.name) then
+            phoneVariation = i
+
+            SetPhoneVariation(i)
+
+            return true
+        end
+    end
+
+    return false
+end
